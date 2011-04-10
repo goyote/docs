21942e14907b30cbe1ce435122a444c664d16874
goyote <goyocode@gmail.com>
2011-04-10 15:56:14 -0700

    Removed unnecessary wording.

--- views/docs/en/start_here/welcome.php
+++ views/docs/en/start_here/welcome.php
@@ -8,6 +8,6 @@
 	<li><strong>Bonus:</strong> Some people suggest that one should act as if one were a compiled binary and parse the source code with the eyeballs.</li>
 </ol>
 
-<p>Point number two is not that bad once you hang out for a while, because you eventually learn where to look and where to ask the right questions. Point number one on the other hand, is a total biatch. Incomplete documentation translates to: <em>"Good luck toolbag, you're on your own now!"</em> How terrible is that? Well turns out it's not. The real problem is you've grown accustomed to getting everything for free, while you give nothing back in return. You're a leach essentially, and deserve nothing.</p>
+<p>Point number two is not that bad once you hang out for a while, because you eventually learn where to look and where to ask the right questions. Point number one on the other hand, is a total biatch. Incomplete documentation translates to: <em>"Good luck, you're on your own now!"</em> How terrible is that? Well turns out it's not. The real problem is you've grown accustomed to getting everything for free, while you give nothing back in return. You're a leach essentially, and deserve nothing.</p>
 
 <p>Since english is the official language of the framework, I'll be writing the docs in english. Spanish is my primary language, so it'll also receive official support. If you're fluent in another language, and want to translate a page, feel free to <a href="http://github.com/goyote/docs">fork the project</a> and send a pull request. gl</p>
