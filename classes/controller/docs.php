f910b5432aac7abb655c7bad569cbe8d3bab1e4e
goyote <goyocode@gmail.com>
2011-04-20 22:10:42 -0700

    Added github "recent updates" section to sidebar. Also made a better 404 page.

--- classes/controller/docs.php
+++ classes/controller/docs.php
@@ -47,12 +47,37 @@ class Controller_Docs extends Controller_Template {
 		try
 		{
 			// Load the article
-			$this->template->content = new View("docs/{$this->lang}/$category/$article");
+			$view = new View("docs/{$this->lang}/$category/$article");
 		}
 		catch (Exception $e)
 		{
-			// Article not found
-			throw new HTTP_Exception_404;
+			// Oh noes!
+			$this->response->status(404);
+			$view = new View('404');
+		}
+
+		$this->template->content = $view;
+
+		if (($feed = Cache::instance()->get('feed')) === NULL)
+		{
+			// GitHub atom feed URL
+			$url = 'https://github.com/goyote/docs/commits/master.atom';
+
+			// Grab the 10 latest entries
+			$entries = Feed::parse($url, 10);
+
+			$feed = array();
+			foreach ($entries as $entry)
+			{
+				$feed[] = array(
+					'title' => $entry['title'],
+					'href' => (string) $entry['link']['href'],
+					'date' => Date::formatted_time($entry['updated'], 'n-d-Y'),
+				);
+			}
+
+			// Cache the entries for 1 day
+			Cache::instance()->set('feed', $feed, Date::DAY);
 		}
 
 		$this->template->set(array(
@@ -62,6 +87,7 @@ class Controller_Docs extends Controller_Template {
 			'navigation' => $this->_config['navigation'],
 			'resources' => Arr::path($this->_config, "resources.$category.$article"),
 			'languages' => $this->_config['language']['supported'],
+			'feed' => $feed,
 		));
 	}
 
