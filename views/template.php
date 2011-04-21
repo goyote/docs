53664591ba898af038ed5235eef50a2d29362c3b
goyote <goyocode@gmail.com>
2011-04-20 22:36:26 -0700

    Fixed html markup for cc license image.

--- views/template.php
+++ views/template.php
@@ -55,7 +55,7 @@
 
 							<?php echo $content ?>
 
-							<a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons Attribution-NonCommercial 3.0 Unported License</a>.
+							<p><a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons Attribution-NonCommercial 3.0 Unported License</a>.</p>
 						</div>
 					</div>
 					<div id="sidebar">
