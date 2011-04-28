ad3f3f7bcd603b69561d42884fadc0b1531d0b93
goyote <goyocode@gmail.com>
2011-04-28 09:38:31 -0700

    Added a bunch of news for 4/28/11.

--- views/docs/en/start_here/welcome.php
+++ views/docs/en/start_here/welcome.php
@@ -4,12 +4,14 @@
 
 <p>Now, on to business. kowut is a warm place where the editor likes to spend his time talking about <a href="http://kohanaframework.org">Kohana</a>. If you find anything useful great, if not that's ok. Done, the intro is over. Go have fun.</p>
 
-<p><strong>News:</strong></p>
+<p><strong>Lots of News:</strong></p>
 
 <ul>
-	<li>I'm about to start work on the next guide: the unittest module!</li>
-	<li>Added <a href="/<?php echo I18n::$lang ?>/helpers/url">URL</a> guide. So far I covered the <code>base()</code> method.</li>
-	<li><a href="/<?php echo I18n::$lang ?>/modules/kostache">Kostache</a> guide started! Pragmas and partials will be covered soon.</li>
+	<li>You might notice some UI changes in the header ;) I'm digging it.</li>
+	<li>Yesterday, I received a msg on GitHub by one of the main devs behind PhpStorm, Alexey Gopachenko! Further proving, there awesomeness.</li>
+	<li>kowut received it's first ever pull request, thanks fo3-nik5!</li>
+	<li>I'm moments away from releasing a full, completed version of the <a href="/<?php echo I18n::$lang ?>/helpers/url">URL</a> guide. I'm proud of it.</li>
+	<li>Expect a major rewrite of the Kostache guide today! I'm already done with half of it.</li>
 </ul>
 	
 <p class="disclaimer"><strong>Disclaimer:</strong></p>
